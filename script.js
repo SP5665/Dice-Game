a810929ad50894bcd83a6956c1dc2a461102318b
Srishti <srishtipandey0512@gmail.com>
2025-07-19 17:10:34 +0530

    dice game script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.random()*6;
+randomNumber1 = Math.floor(randomNumber1)+1;
+var randomNumber2 = Math.random()*6;
+randomNumber2 = Math.floor(randomNumber2)+1;
+
+var randomImage1 = "./images/dice"+randomNumber1+".png";
+var randomImage2 = "./images/dice"+randomNumber2+".png";
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImage1);
+image2.setAttribute("src", randomImage2);
+
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if(randomNumber1<randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else {
+    document.querySelector("h1").innerHTML="Tie";
+}
